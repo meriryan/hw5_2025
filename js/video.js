efe6bb79d9681d1921c07a31fa4e9779068a0f2a
meriryan <meriryan@umich.edu>
2025-11-23 18:32:50 -0500

    Add files via upload

---
+++ js/video.js
@@ -0,0 +1,70 @@
+var video;
+
+window.addEventListener("load", function() {
+	console.log("Good job opening the window")
+
+});
+
+window.addEventListener("load", function() {
+	console.log("Good job opening the window");
+	
+	video = document.querySelector("#player1");
+	video.autoplay = false;
+	video.loop = false;
+	
+	document.querySelector("#volume").textContent = video.volume * 100 + "%";
+});
+
+document.querySelector("#play").addEventListener("click", function() {
+	console.log("Play Video");
+	video.play();
+	document.querySelector("#volume").textContent = video.volume * 100 + "%";
+});
+
+document.querySelector("#pause").addEventListener("click", function() {
+	console.log("Pause Video");
+	video.pause();
+});
+
+document.querySelector("#slower").addEventListener("click", function() {
+	video.playbackRate *= 0.9;
+	console.log("Speed is: " + video.playbackRate);
+});
+
+document.querySelector("#faster").addEventListener("click", function() {
+	video.playbackRate /= 0.9;
+	console.log("Speed is: " + video.playbackRate);
+});
+
+document.querySelector("#skip").addEventListener("click", function() {
+	video.currentTime += 10;
+	
+	if (video.currentTime >= video.duration) {
+		video.currentTime = 0;
+	}
+	
+	console.log("Current time is: " + video.currentTime);
+});
+
+document.querySelector("#mute").addEventListener("click", function() {
+	if (video.muted) {
+		video.muted = false;
+		document.querySelector("#mute").textContent = "Mute";
+	} else {
+		video.muted = true;
+		document.querySelector("#mute").textContent = "Unmute";
+	}
+});
+
+document.querySelector("#slider").addEventListener("input", function() {
+	video.volume = this.value / 100;
+	document.querySelector("#volume").textContent = video.volume * 100 + "%";
+});
+
+document.querySelector("#vintage").addEventListener("click", function() {
+	video.classList.add("oldSchool");
+});
+
+document.querySelector("#orig").addEventListener("click", function() {
+	video.classList.remove("oldSchool");
+});
